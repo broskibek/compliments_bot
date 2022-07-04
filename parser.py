5c1b7c08c68717252ce1306257402ab9a99d4c2d
broskibek <44233523+broskibek@users.noreply.github.com>
2022-07-04 20:23:49 +0600

    Files added

---
+++ parser.py
@@ -0,0 +1,34 @@
+cat parser.py
+import requests
+import json
+
+list_of_compliments = []
+
+url = 'https://castlots.org/generator-komplimentov-devushke/generate.php'
+
+headers = {
+'Accept': 'application/json, text/javascript, */*; q=0.01',
+'Origin': 'https://castlots.org',
+'Referer': 'https://castlots.org/generator-komplimentov-devushke/',
+'Accept-Language': 'ru',
+'Host': 'castlots.org',
+'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
+'Content-Length': '0',
+'Accept-Encoding': 'gzip, deflate, br',
+'Connection': 'keep-alive',
+'X-Requested-With': 'XMLHttpRequest'
+}
+
+
+for i in range(50):
+        res = requests.get(url, headers = headers)
+        parsed_text = json.loads(res.text)
+        list_of_dict_values = list(parsed_text.values())
+        # print(list_of_dict_values[1])
+        if list_of_dict_values[1] not in list_of_compliments:
+                list_of_compliments.append(list_of_dict_values[1])
+        #       print(len(list_of_compliments))
+        else:
+                pass
+        # print(list_of_compliments)
+#print(list_of_compliments)
