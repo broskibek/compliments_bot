d2bfaad9da0ae35ecccbc1ae925c5b261ce2678a
broskibek <44233523+broskibek@users.noreply.github.com>
2022-07-04 20:24:54 +0600

    Update compliments_bot.py

--- compliments_bot.py
+++ compliments_bot.py
@@ -6,15 +6,13 @@ import secrets
 import time
 from parser import list_of_compliments
 
-bot = telebot.TeleBot("2077649530:AAG-ibfXJxLStNsvwgU85P_AR-5ecGZy51s")
+#add your bot token below
+bot = telebot.TeleBot("x.x.x.x")
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(
-        message.chat.id,
-        'Привет, Лёль) Я комплимент бот написанный Лякой специально для тебя, если ты запустила этот бот, то значит ты соскучилась по нему, \
-        а он работает и не может ответить тебе. Я буду радовать тебя комплиментами при любом твоем сообщении пока Ляка работает на работе мечты)'
-  )
+        message.chat.id, 'Hi it is a bot to write you a compliments for ewvery your message, enjoy')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
