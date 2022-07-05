d44768e31abcf8397fafee10fd2043fa1f71179f
broskibek <44233523+broskibek@users.noreply.github.com>
2022-07-05 10:41:07 +0600

    Update compliments_bot.py

--- compliments_bot.py
+++ compliments_bot.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot("x.x.x.x")
 @bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(
-        message.chat.id, 'Hi it is a bot to write you a compliments for ewvery your message, enjoy')
+        message.chat.id, 'Hi, it\'s a bot to write you a compliments for every your message, enjoy')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
